738835634cca1a41b43811b20f9948181425b4d8
David Goffredo <dmgoffredo@gmail.com>
2019-04-11 23:43:35 -0400

    this will help to radix sort aribtrary integers

---
+++ morgan-and-a-string/numeric.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(require srfi/43) ; vector-reverse-copy
+
+(define (integer->vector nonnegative-integer
+                         num-bytes 
+                         #:endianness [endianness 'big] 
+                         #:bits-per-byte [bits-per-byte 8])
+  "Return a vector containing the lowest 'num-bytes' of a byte-wise
+   representation of 'nonnegative-integer'. The resulting bytes will be
+   arranged according to the 'endianness'. Each byte is a nonnegative integer."
+  (if (not (nonnegative-integer? nonnegative-integer))
+    (raise-argument-error
+      'nonnegative-integer "nonnegative-integer?" nonnegative-integer)
+    (let* ([mask (sub1 (expt 2 bits-per-byte))] ; e.g. 0xFF
+           [bytes-vector
+            (for/vector ([i (in-range num-bytes)])
+              (let* ([shift (- (* i bits-per-byte))]
+                     [shifted (arithmetic-shift nonnegative-integer shift)])
+                (bitwise-and shifted mask)))])
+      (match endianness
+        ['little bytes-vector]
+        ['big    (vector-reverse-copy bytes-vector)]))))
